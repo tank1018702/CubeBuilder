1d6b4aa2c750f8a70dac64e3e1c4640afadcb248
shenyan <tank1018702@sina.com>
2019-04-04 17:43:27 +0800

    cube

---
+++ New Unity Project (3)/Assets/Scripts/CubeMetrics.cs
@@ -0,0 +1,180 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeMetrics : MonoBehaviour
+{
+   public static Vector3[] cubeVertex =
+   { 
+        //上面四个顶点
+        //左上
+        new Vector3(-0.5f,0.5f,0.5f),
+        //右上
+        new Vector3(0.5f,0.5f,0.5f),
+        //右下
+        new Vector3 (0.5f,0.5f,-0.5f),
+        //左下
+        new Vector3(-0.5f,0.5f,-0.5f),
+        //下面四个顶点
+        //左上
+        new Vector3(-0.5f,-0.5f,0.5f),
+        //右上
+        new Vector3(0.5f,-0.5f,0.5f),
+        //右下
+        new Vector3(0.5f,-0.5f,-0.5f),
+        //左下
+        new Vector3(-0.5f,-0.5f,-0.5f)
+    };
+
+    public static Vector3 GetCubePosByDirection(Vector3 pos,CubeNeighborDirection direction)
+    {  
+        switch (direction)
+        {
+            case CubeNeighborDirection.front:
+                pos += Vector3.forward;
+                break;
+            case CubeNeighborDirection.frontUp:
+                pos += Vector3.forward + Vector3.up;
+                break;
+            case CubeNeighborDirection.frontDown:
+                pos += Vector3.forward + Vector3.down;
+                break;
+            case CubeNeighborDirection.back:
+                pos += Vector3.back;
+                break;
+            case CubeNeighborDirection.backUp:
+                pos += Vector3.back + Vector3.up;
+                break;
+            case CubeNeighborDirection.backDown:
+                pos += Vector3.back + Vector3.down;
+                break;
+            case CubeNeighborDirection.left:
+                pos += Vector3.left;
+                break;
+            case CubeNeighborDirection.leftUp:
+                pos += Vector3.left + Vector3.up;
+                break;
+            case CubeNeighborDirection.leftDown:
+                pos += Vector3.left + Vector3.down;
+                break;
+            case CubeNeighborDirection.right:
+                pos += Vector3.right;
+                break;
+            case CubeNeighborDirection.rightUp:
+                pos += Vector3.right + Vector3.up;
+                break;
+            case CubeNeighborDirection.rightDown:
+                pos += Vector3.right + Vector3.down;
+                break;
+            case CubeNeighborDirection.up:
+                pos += Vector3.up;
+                break;
+            case CubeNeighborDirection.dowm:
+                pos += Vector3.down;
+                break;
+                
+        }
+        return pos;
+    }
+
+    public static CubeNeighborDirection GetOppositeDirection(CubeNeighborDirection direction)
+    {
+        switch(direction)
+        {
+            case CubeNeighborDirection.front:
+                return CubeNeighborDirection.back;
+            case CubeNeighborDirection.frontUp:
+                return CubeNeighborDirection.backDown;
+            case CubeNeighborDirection.frontDown:
+                return CubeNeighborDirection.backUp;
+
+            case CubeNeighborDirection.back:
+                return CubeNeighborDirection.front;
+            case CubeNeighborDirection.backUp:
+                return CubeNeighborDirection.frontDown;
+            case CubeNeighborDirection.backDown:
+                return CubeNeighborDirection.frontUp;
+
+            case CubeNeighborDirection.left:
+                return CubeNeighborDirection.right;
+            case CubeNeighborDirection.leftUp:
+                return CubeNeighborDirection.rightDown;
+            case CubeNeighborDirection.leftDown:
+                return CubeNeighborDirection.rightUp;
+
+            case CubeNeighborDirection.right:
+                return CubeNeighborDirection.left;
+            case CubeNeighborDirection.rightUp:
+                return CubeNeighborDirection.leftDown;
+            case CubeNeighborDirection.rightDown:
+                return CubeNeighborDirection.leftUp;
+
+            case CubeNeighborDirection.up:
+                return CubeNeighborDirection.dowm;
+            case CubeNeighborDirection.dowm:
+                return CubeNeighborDirection.up;
+
+            default:
+                return direction;
+        }
+    }
+
+    public static Vector3 FromWorldPositionToCubePosition(Vector3 position)
+    {
+        Vector3 resut = Vector3.zero;
+        resut.x = position.x > 0 ? (int)position.x * 1f + 0.5f : (int)position.x * 1f - 0.5f;
+        resut.y = position.y > 0 ? (int)position.y * 1f + 0.5f : (int)position.y * 1f - 0.5f;
+        resut.z = position.z > 0 ? (int)position.z * 1f + 0.5f : (int)position.z * 1f - 0.5f;
+        return resut;
+    }
+
+
+}
+public enum CubeNeighborDirection
+{
+    front,
+    frontUp,
+    frontDown,
+
+    back,
+    backUp,
+    backDown,
+
+    left,
+    leftUp,
+    leftDown,
+
+    right,
+    rightUp,
+    rightDown,
+
+    up,
+    dowm,
+}
+
+public enum M_CubeType
+{
+    test1,
+    test2,
+    test3,
+    test4,
+    test5,
+    test6
+}
+
+public enum CubeSurface
+{
+    front, right, back, left, up, down
+}
+
+public enum CubeOrientate
+{
+    front, right, back, left
+}
+
+public enum KeyDirection
+{
+    up, down, left, right
+}
+
+
---
+++ New Unity Project (3)/Assets/Scripts/MapEditor.cs
@@ -0,0 +1,122 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapEditor : MonoBehaviour
+{
+    public CubeMesh cubeMesh,preview;
+
+   
+
+    M_CubeType cubeType = M_CubeType.test1;
+
+    CubeOrientate Orientate;
+
+
+
+    void Start()
+    {
+        preview.UpdateCube(cubeType,Orientate);
+    }
+
+
+    public void TypeSelect(int type)
+    {
+        cubeType = (M_CubeType)type;
+        preview.UpdateCube(cubeType,Orientate);
+    }
+
+
+    void Update()
+    {
+        PreviewControl();
+        if(OrientateControl())
+        {
+            preview.UpdateCube(cubeType, Orientate);
+        }
+       
+    }
+
+    bool GetMouseRayPoint(out Vector3 addCubePosition, out Vector3 removeCubePosition)
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hitInfo;
+        if (Physics.Raycast(ray, out hitInfo))
+        {
+
+            Debug.DrawRay(hitInfo.point, Vector3.up, Color.red);
+
+            addCubePosition = CubeMetrics.FromWorldPositionToCubePosition(hitInfo.point - ray.direction * 0.001f);
+            removeCubePosition = CubeMetrics.FromWorldPositionToCubePosition(hitInfo.point + ray.direction * 0.001f);
+            return true;
+        }
+        addCubePosition = Vector3.zero;
+        removeCubePosition = Vector3.zero;
+        return false;
+    }
+
+    bool OrientateControl()
+    {
+        CubeOrientate temp = Orientate;
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            Orientate = (int)Orientate == 0 ? (CubeOrientate)3 : (CubeOrientate)Orientate - 1;
+        }
+        else if (Input.GetKeyDown(KeyCode.E))
+        {
+            Orientate = (int)Orientate == 3 ? (CubeOrientate)0 : (CubeOrientate)Orientate + 1;
+        }
+
+        if(temp!=Orientate)
+        {
+            return true;
+        }
+
+        return false;
+    }
+
+    void PreviewControl()
+    {
+        Vector3 addCubePosition;
+        Vector3 removeCubePosition;
+        if (GetMouseRayPoint(out addCubePosition, out removeCubePosition))
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+
+                cubeMesh.AddCube(addCubePosition, cubeType,Orientate);
+            }
+            else if (Input.GetMouseButtonDown(1))
+            {
+                Debug.DrawRay(removeCubePosition, Vector3.up, Color.blue, 1f);
+                cubeMesh.RemoveCube(removeCubePosition);
+            }
+            else
+            {
+                preview.transform.position = addCubePosition;
+            }
+        }
+        else
+        {
+            preview.transform.position = Vector3.down;
+        }
+
+    }
+
+
+    private void OnDrawGizmos()
+    {
+        
+        if (GetMouseRayPoint(out Vector3 cubePosition1,out Vector3 cubePosition2))
+        {
+            Gizmos.color = Color.blue;
+            Gizmos.DrawWireCube(cubePosition2, Vector3.one);
+            Gizmos.color = Color.white;
+            Gizmos.DrawWireCube(cubePosition1, Vector3.one);
+        }
+        
+    }
+
+
+
+}
